ed7e2c633bb7693af59a32ae23546f812e959c60
Asmaa Elkholy <asmaaelkholy6@gmail.com>
2025-01-30 11:02:54 +0300

    Saving changes before pull

---
+++ MOILibrary/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MOILibrary
+//
+//  Created by asmaa4050 on 01/30/2025.
+//  Copyright (c) 2025 asmaa4050. All rights reserved.
+//
+
+import UIKit
+import MOILibrary
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
---
+++ Pods/Target Support Files/MOILibrary/MOILibrary-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_MOILibrary : NSObject
+@end
+@implementation PodsDummy_MOILibrary
+@end
